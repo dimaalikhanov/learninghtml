c49a613c004b44639c1f12065aef49898b34dd21
dimaalikhanov <dima.alikhanov1992@gmail.com>
2021-11-04 00:22:26 +0600

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-let a, b;
-a = 2;
-b = 3;
+let car1 = {
+  color: 'green',
+  maxSpeed: '250',
+  brand: {
+    brand: 'toyota',
+    model: 'camry'
 
-console.log(a + b);
+  }
+  }
+
+let car2 = {
+  color: 'red',
+  maxSpeed: '270',
+  brand: {
+    brand: 'nissan',
+    model: 'teana'
+  }
+}
+
+car1.color = car2.color;
+
+console.log(car1, car2);
